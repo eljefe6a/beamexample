e7158103be465b5cad20533fd39271ad9c6b48fd
Jesse Anderson <jesse@smokinghand.com>
2016-09-15 09:51:20 -0700

    Changed to write to a single file. Added commenting.

--- BeamTutorial/src/main/java/org/apache/beam/examples/tutorial/game/utils/UnboundedWriteIO.java
+++ BeamTutorial/src/main/java/org/apache/beam/examples/tutorial/game/utils/UnboundedWriteIO.java
@@ -1,56 +1,80 @@
 package org.apache.beam.examples.tutorial.game.utils;
 
+import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.channels.Channels;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
 import org.apache.beam.sdk.util.IOChannelUtils;
 import org.apache.beam.sdk.util.MimeTypes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
+/**
+ * Note: this class shouldn't be used for production code. It is used for
+ * debugging and working locally on Beam pipelines.
+ *
+ */
 public class UnboundedWriteIO extends DoFn<String, Void> {
-  private transient Map<String, List<String>> outputs;
+  private static final Logger LOG = LoggerFactory.getLogger(UnboundedWriteIO.class);
 
+  /** List of output data to write */
+  private transient List<String> outputs;
+
+  /** Stream to write out file to */
+  private transient BufferedOutputStream stream;
+
+  /** Prefix of the file to write */
   protected final String prefix;
 
+  /**
+   * Constructor
+   * 
+   * @param prefix
+   *          Prefix of the file to write
+   */
   public UnboundedWriteIO(String prefix) {
     this.prefix = prefix;
   }
 
   @StartBundle
   public void startBundle(Context c) {
-    outputs = Maps.newHashMap();
+    outputs = Lists.newArrayList();
   }
 
   @ProcessElement
   public void processElement(ProcessContext c, BoundedWindow w) throws Exception {
-    String filename = new StringBuilder().append(prefix).append("/").append(w.toString()).toString();
-    List<String> values = outputs.get(filename);
-    if (values == null) {
-      values = Lists.newArrayList();
-      outputs.put(filename, values);
-    }
-
-    values.add(c.element());
+    outputs.add(w.toString() + " : " + c.element());
   }
 
   @FinishBundle
   public void finishBundle(Context c) throws IOException {
-    for (String filename : outputs.keySet()) {
-      OutputStream out = Channels.newOutputStream(IOChannelUtils.create(filename, MimeTypes.TEXT));
-      for (String v : outputs.get(filename)) {
-        out.write(v.getBytes(StandardCharsets.UTF_8));
-        out.write("\n".getBytes(StandardCharsets.UTF_8));
-      }
-      out.close();
+    // Check if stream has been created yet. Initialize if not.
+    if (stream == null) {
+      stream = new BufferedOutputStream(Channels.newOutputStream(IOChannelUtils.create(prefix, MimeTypes.TEXT)));
     }
+
+    // Write out all outputs
+    for (String output : outputs) {
+      stream.write(output.getBytes(StandardCharsets.UTF_8));
+      stream.write("\n".getBytes(StandardCharsets.UTF_8));
+    }
+
+    // Clear outputs as they've been written
     outputs.clear();
+  }
+
+  @Teardown
+  public void teardown() {
+    try {
+      stream.close();
+    } catch (IOException e) {
+      LOG.error("Failed to close output file.", e);
+    }
   }
 }
